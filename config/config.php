c8f3f1114bd5ae3278fca65bef558b65c07a9627
Crash-Zeus <jonas.bertin49@gmail.com>
2019-10-27 13:57:09 +0100

    switch array to []

--- config/config.php
+++ config/config.php
@@ -1,9 +1,7 @@
 <?php
-return array (
-
+return [
 	"db_user" => "le_nom_de_l'utilisateur_bdd",
 	"db_password" => "le_mdp_de_l'utilisateur_bdd",
 	"db_host" => "localhost",
 	"db_name" => "le_db_name_de_la_bdd_utiliser"
-
-);
+];
