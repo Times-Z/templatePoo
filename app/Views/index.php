275de3f278758cbc3dc9172ee788cb9446b377e5
Crash-Zeus <jonas.bertin49@gmail.com>
2019-11-04 20:18:51 +0100

    fix w3c error

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,5 +1,5 @@
 <div class="row  justify-content-md-center">
-    <form action="" method="post">
+    <form action="#" method="post">
         <?= $form->input('login', 'Login', [], 'Login')?>
         <?= $form->password('password', 'password', 'password', false)?>
         <?= $form->submit('submit', 'envoyer')?>
