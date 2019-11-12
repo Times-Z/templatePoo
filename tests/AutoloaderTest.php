1c1de4eae056f128b969c5413785cc86b4614121
Crash-Zeus <jonas.bertin49@gmail.com>
2019-11-12 13:42:50 +0100

    fix test autoloader

--- tests/AutoloaderTest.php
+++ tests/AutoloaderTest.php
@@ -6,10 +6,31 @@ use PHPUnit\Framework\TestCase;
 final class AutoloaderTest extends TestCase {
 
     public function testRegister() {
-        $this->assertIsBool(\App\Autoloader::register());
+        $config = \App\Autoloader::register();
+
+        $this->assertIsBool($config);
+        $this->assertIsNotString($config);
+        $this->assertIsScalar($config);
+        $this->assertIsNotIterable($config);
+        $this->assertIsNotObject($config);
+        $this->assertIsNotArray($config);
+        $this->assertIsNotCallable($config);
+        $this->assertIsNotFloat($config);
+        $this->assertIsNotInt($config);
     }
 
     public function testAutoload() {
-        $this->assertNull(\App\Autoloader::autoload('App'));
+        $config = \App\Autoloader::autoload('App');
+
+        $this->assertNull($config);
+        $this->assertIsNotBool($config);
+        $this->assertIsNotString($config);
+        $this->assertIsNotScalar($config);
+        $this->assertIsNotIterable($config);
+        $this->assertIsNotObject($config);
+        $this->assertIsNotArray($config);
+        $this->assertIsNotCallable($config);
+        $this->assertIsNotFloat($config);
+        $this->assertIsNotInt($config);
     }
 }
