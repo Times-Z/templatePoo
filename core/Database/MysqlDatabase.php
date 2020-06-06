445eeac63de813b4b4ff60e340f951b26e1297e3
Crash-Zeus <jonas.bertin49@gmail.com>
2020-06-06 13:56:34 +0200

    Fixed some return type

--- core/Database/MysqlDatabase.php
+++ core/Database/MysqlDatabase.php
@@ -14,7 +14,7 @@ Class MysqlDatabase extends Database {
 	private string $db_host;
 	private $pdo;
 
-    public function __construct(?string $db_name, ?string $db_user = 'local', ?string $db_password = 'admin', ?string $db_host = 'localhost') {
+    public function __construct(?string $db_name, ?string $db_user = 'root', ?string $db_password = 'root', ?string $db_host = 'db') {
 		$this->db_name = $db_name;
 		$this->db_user = $db_user;
 		$this->db_password = $db_password;
@@ -26,7 +26,7 @@ Class MysqlDatabase extends Database {
 	 *
 	 * @return PDO
 	 */
-	private function getPDO() {
+	private function getPDO() :PDO {
 		if ($this->pdo === null) {
 			$pdo = new PDO('mysql:dbname=' . $this->db_name . ';host=' . $this->db_host . '', '' . $this->db_user . '', '' . $this->db_password . '');
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
