5e6cccbd7fb47263da3bd55db6dde1ff97f88645
Crash-Zeus <jonas.bertin49@gmail.com>
2020-06-06 13:56:26 +0200

    Fixed some return type

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App;
 use Core\HTML\BootstrapForm;
 
 class IndexController extends AppController {
@@ -10,17 +11,32 @@ class IndexController extends AppController {
         parent::__construct();
     }
 
+    /**
+     * Function rendering index page with form var
+     *
+     * @return void
+     */
     public function index() :void {
         $form = new BootstrapForm($_POST);
         $this->render('index', compact('form'));
     }
 
+    /**
+     * Function redering other page
+     *
+     * @return void
+     */
     public function other() :void {
         $this->render('other');
     }
 
+    /**
+     * Function rendering post page with new title
+     *
+     * @return void
+     */
     public function post() :void{
-        \App::getInstance()->title = "NEVER TRUST USER INPUT";
+        App::getInstance()->title = "NEVER TRUST USER INPUT";
         $this->render('template');
     }
 }
